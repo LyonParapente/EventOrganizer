62029a11faaefe095371f4602f481c979db2c31c
Mickaël FRANC <mickael.franc@gmail.com>
2018-08-10 01:07:28 +0200

    initialize the templating architecture

--- event_organizer.go
+++ event_organizer.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/LyonParapente/EventOrganizer/core"
 	"github.com/LyonParapente/EventOrganizer/srv"
+	"github.com/LyonParapente/EventOrganizer/templates"
 )
 
 type EventOrganizer struct {
@@ -15,10 +16,19 @@ type EventOrganizer struct {
 }
 
 func (e *EventOrganizer) Start() error {
-	router := http.NewServeMux()
-	router.Handle("/", http.FileServer(http.Dir(e.PublicPath)))
-	router.Handle("/events", &srv.GetEvents{App: e})
-
+	e.GetLogger().Debug("Initialize templating...")
+	templates.LoadAllOrFail()
 	e.GetLogger().Info("Listen on", e.Target, "...")
-	return http.ListenAndServe(e.Target, router)
+	return http.ListenAndServe(e.Target, e.router())
+}
+
+// router initialize routings
+func (e *EventOrganizer) router() http.Handler {
+	router := http.NewServeMux()
+	// router.Handle("/", http.FileServer(http.Dir(e.PublicPath)))
+	router.Handle("/", &srv.GetHomepage{App: e})
+	router.Handle("/calendar", &srv.GetCalendar{App: e})
+
+	router.Handle("/api/events", &srv.GetEvents{App: e})
+	return router
 }
