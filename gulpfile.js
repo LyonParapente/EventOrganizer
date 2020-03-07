3cf5639e49646bed4c505b39240d9f17d4d1bde8
TiTi <anthibug@gmail.com>
2020-03-07 20:18:20 +0100

    Switch to browser-sync which is way better
    - browser auto reload (after compilation tasks) when a source is changed
    - sync multiple browsers
    - less complicated than watchify to set up

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require("gulp");
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
 var browserify = require("browserify");
-var watchify = require("watchify");
 var source = require('vinyl-source-stream');
 var tsify = require("tsify");
 var uglify = require('gulp-uglify-es').default;
@@ -10,40 +9,34 @@ var sourcemaps = require('gulp-sourcemaps');
 var buffer = require('vinyl-buffer');
 var sass = require('gulp-sass');
 sass.compiler = require('node-sass');
+const browserSync = require('browser-sync').create();
 
 var dist = "dist";
 
-gulp.task('copy-html', function ()
+gulp.task('copy html', function ()
 {
 	return gulp.src('src/*.html')
 		.pipe(gulp.dest(dist));
 });
 
-var b =  browserify(
-{
-	basedir: '.',
-	debug: true,
-	entries: ['src/main.ts'],
-	cache: {},
-	packageCache: {},
-	plugin: [watchify]
-});
-b.on('update', bundlejs);
-//b.on("update", bundlecss);
-b.on("log", console.log);
-
 function bundlejs()
 {
-	return b
-		.plugin(tsify)
-		.bundle()
-		.on('error', console.error)
-		.pipe(source('bundle.min.js'))
-		.pipe(buffer())
-		.pipe(sourcemaps.init({loadMaps: true}))
-		.pipe(uglify())
-		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest(dist));
+	return browserify(
+	{
+		basedir: '.',
+		debug: true,
+		entries: ['src/main.ts'],
+		cache: {},
+		packageCache: {}
+	})
+	.plugin(tsify)
+	.bundle()
+	.pipe(source('bundle.min.js'))
+	.pipe(buffer())
+	.pipe(sourcemaps.init({loadMaps: true}))
+	.pipe(uglify())
+	.pipe(sourcemaps.write('./'))
+	.pipe(gulp.dest(dist));
 }
 
 gulp.task("copy fontawesome", function()
@@ -92,5 +85,27 @@ function bundlecss()
 {
 	return gulp.parallel("copy fontawesome", "copy css themes", "copy webfonts", "fullcalendar css", "scss");
 }
-gulp.task("sass", bundlecss());
-gulp.task("default", gulp.series('copy-html', gulp.parallel(bundlejs, "sass")));
+gulp.task("css", bundlecss());
+
+gulp.task('serve', function()
+{
+	browserSync.init(
+	{
+		server:
+		{
+			baseDir: "dist",
+			index: "calendar.html",
+			routes:
+			{
+				"/events": "data/events",
+				"/avatars": "data/avatars"
+			}
+		}
+	});
+	
+	gulp.watch("src/*.html").on('change', gulp.series('copy html', browserSync.reload));
+	gulp.watch("src/**/*.ts").on('change', gulp.series(bundlejs, browserSync.reload));
+	gulp.watch(["src/css/**/*.scss", "src/css/**/*.css"]).on('change', gulp.series("css", browserSync.reload));
+});
+
+gulp.task("default", gulp.series("copy html", gulp.parallel(bundlejs, "css"), "serve"));
