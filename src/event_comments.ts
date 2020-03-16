6da1108ddbbc9e08cdf1979d0ed8e1d99b34269f
TiTi <anthibug@gmail.com>
2020-03-16 19:03:18 +0100

    Refactor event_comments.ts, cut into several functions

--- src/event_comments.ts
+++ src/event_comments.ts
@@ -13,125 +13,134 @@ export default function loadComments(id, isFinished)
 	});
 
 	var event_comments = document.getElementById('event_comments');
-	var comments_spinner = document.createElement('div');
-	comments_spinner.setAttribute("role", "status");
-	comments_spinner.className = "spinner-border m-auto";
-	event_comments.innerHTML = '';
-	event_comments.appendChild(comments_spinner);
+	event_comments.innerHTML = '<div class="spinner-border m-auto" role="status"></div>';
 
-	function removeCommentsSpinner()
-	{
-		event_comments.removeChild(comments_spinner);
-	}
-
-	var $event_participants = $("#event_participants").empty();
-	var $event_interested = $("#event_interested").empty();
+	var $participants = $("#event_participants").empty();
+	var $interested = $("#event_interested").empty();
 
 	requestJson("GET", "events/Event_"+id+".json", null, function(data)
 	{
-		removeCommentsSpinner();
-		var a, i, avatar;
-		event_comments.innerHTML = ''; // rendering optim to avoid repaint in .always()
-		for (i = 0; i < data.comments.length; ++i)
-		{
-			var comment = data.comments[i],
-				userid = comment.user,
-				username = data.users[userid];
-
-			if (!username)
-			{
-				console.warn("Missing user "+userid);
-			}
-
-			var dateText = toRelativeTimeString(new Date(comment.date));
-
-			var groupitem = document.createElement('div');
-			groupitem.className = 'list-group-item p-1 d-flex';
-				var d = document.createElement('div');
-					a = document.createElement('a');
-					a.href = "user/"+userid;
-						avatar = new Image();
-						avatar.src = "avatars/"+userid+"-2.jpg";
-						avatar.alt = username;
-					a.appendChild(avatar);
-				d.appendChild(a);
-				groupitem.appendChild(d);
-
-				var col = document.createElement('div');
-				col.className = 'col';
-					var comment_infos = document.createElement('span');
-					comment_infos.className = 'border-bottom border-light';
-						a = document.createElement('a');
-						a.href = "user/"+userid;
-						a.appendChild(document.createTextNode(username));
-					comment_infos.appendChild(a);
-					comment_infos.appendChild(document.createTextNode(' - ' + dateText));
-
-					col.appendChild(comment_infos);
-					var p = document.createElement("p");
-					p.className = 'blockquote my-1 text-dark';
-					p.appendChild(document.createTextNode(comment.comment));
-					p.innerHTML = p.innerHTML.replace(/\n/g, '<br/>');
-				col.appendChild(p);
-			groupitem.appendChild(col);
-			event_comments.appendChild(groupitem);
-		}
-
-		data.participants = data.participants || [];
-		var participants_badge = $('<span class="badge badge-success"></span>').text(data.participants.length);
-		var participants_button = isFinished ? '' : $('<button type="button" class="btn btn-outline-info float-right"></button>').text(i18n("I'm in"));
-		var participants_header = $("<h4>").text(i18n("Participants ")).append(participants_badge).append(participants_button);
-		$event_participants.append(participants_header);
-		for (i = 0; i < data.participants.length; ++i)
-		{
-			var participant = data.participants[i];
-			if (data.users.hasOwnProperty(participant))
-			{
-				a = document.createElement('a');
-				a.href = "user/"+participant;
-					avatar = new Image();
-					avatar.src = "avatars/"+participant+"-2.jpg";
-					avatar.alt = data.users[participant];
-					avatar.className = "mr-1 mb-1";
-				a.appendChild(avatar);
-				$event_participants.append(a);
-			}
-			else
-			{
-				console.warn("Missing participant user "+participant);
-			}
-		}
-
-		data.interested = data.interested || [];
-		var interested_badge = $('<span class="badge badge-info"></span>').text(data.interested.length);
-		var interested_button = isFinished ? '' : $('<button type="button" class="btn btn-outline-info float-right"></button>').text(i18n("I'm interested"));
-		var interested_header = $("<h4>").text(i18n("Interested ")).append(interested_badge).append(interested_button);
-		$event_interested.append(interested_header);
-		for (i = 0; i < data.interested.length; ++i)
-		{
-			var interested = data.interested[i];
-			if (data.users[interested])
-			{
-				a = document.createElement('a');
-				a.href = "user/"+interested;
-					avatar = new Image();
-					avatar.src = "avatars/"+interested+"-2.jpg";
-					avatar.alt = data.users[interested];
-					avatar.className = "mr-1 mb-1";
-				a.appendChild(avatar);
-				$event_interested.append(a);
-			}
-			else
-			{
-				console.warn("Missing interested user "+interested);
-			}
-		}
+		event_comments.innerHTML = ''; // Remove spinner
+		receiveEventInfos(data, event_comments, isFinished, $participants, $interested);
 	},
 	function(type, ex)
 	{
+		event_comments.innerHTML = ''; // Remove spinner
 		console.error(type, ex);
 		$errorBox.show();
-		$errorBox.clone().removeAttr("id").appendTo($event_participants);
-		removeCommentsSpinner();
+		$errorBox.clone().removeAttr("id").appendTo($participants);
 	});
+}
+
+
+function receiveEventInfos(data, event_comments, isFinished, $participants, $interested)
+{
+	// Comments
+	for (var i = 0; i < data.comments.length; ++i)
+	{
+		var comment = data.comments[i],
+			userid = comment.user,
+			username = data.users[userid];
+
+		if (!username)
+		{
+			console.warn("Missing user "+userid);
+		}
+
+		var groupitem = createCommentEntry(comment, userid, username);
+		event_comments.appendChild(groupitem);
+	}
+
+	createParticipants(data.participants || [], data.users, isFinished, $participants);
+	createInterested(data.interested || [], data.users, isFinished, $interested);
+}
+
+function createCommentEntry(comment, userid, username)
+{
+	var dateText = toRelativeTimeString(new Date(comment.date));
+
+	var groupitem = document.createElement('div');
+	groupitem.className = 'list-group-item p-1 d-flex';
+		var d = document.createElement('div');
+			var a = document.createElement('a');
+			a.href = "user/"+userid;
+				var avatar = new Image();
+				avatar.src = "avatars/"+userid+"-2.jpg";
+				avatar.alt = username;
+			a.appendChild(avatar);
+		d.appendChild(a);
+		groupitem.appendChild(d);
+
+		var col = document.createElement('div');
+		col.className = 'col';
+			var comment_infos = document.createElement('span');
+			comment_infos.className = 'border-bottom border-light';
+				a = document.createElement('a');
+				a.href = "user/"+userid;
+				a.appendChild(document.createTextNode(username));
+			comment_infos.appendChild(a);
+			comment_infos.appendChild(document.createTextNode(' - ' + dateText));
+
+			col.appendChild(comment_infos);
+			var p = document.createElement("p");
+			p.className = 'blockquote my-1 text-dark';
+			p.appendChild(document.createTextNode(comment.comment));
+			p.innerHTML = p.innerHTML.replace(/\n/g, '<br/>');
+		col.appendChild(p);
+	groupitem.appendChild(col);
+	return groupitem;
+}
+
+function createParticipants(participants, users, isFinished, $event_participants)
+{
+	var participants_badge = $('<span class="badge badge-success"></span>').text(participants.length);
+	var participants_button = isFinished ? '' : $('<button type="button" class="btn btn-outline-info float-right"></button>').text(i18n("I'm in"));
+	var participants_header = $("<h4>").text(i18n("Participants ")).append(participants_badge).append(participants_button);
+	$event_participants.append(participants_header);
+	for (var i = 0; i < participants.length; ++i)
+	{
+		var participant = participants[i];
+		if (users.hasOwnProperty(participant))
+		{
+			var a = document.createElement('a');
+			a.href = "user/"+participant;
+				var avatar = new Image();
+				avatar.src = "avatars/"+participant+"-2.jpg";
+				avatar.alt = users[participant];
+				avatar.className = "mr-1 mb-1";
+			a.appendChild(avatar);
+			$event_participants.append(a);
+		}
+		else
+		{
+			console.warn("Missing participant user "+participant);
+		}
+	}
+}
+
+function createInterested(interested, users, isFinished, $event_interested)
+{
+	var interested_badge = $('<span class="badge badge-info"></span>').text(interested.length);
+	var interested_button = isFinished ? '' : $('<button type="button" class="btn btn-outline-info float-right"></button>').text(i18n("I'm interested"));
+	var interested_header = $("<h4>").text(i18n("Interested ")).append(interested_badge).append(interested_button);
+	$event_interested.append(interested_header);
+	for (var i = 0; i < interested.length; ++i)
+	{
+		var interested = interested[i];
+		if (users[interested])
+		{
+			var a = document.createElement('a');
+			a.href = "user/"+interested;
+				var avatar = new Image();
+				avatar.src = "avatars/"+interested+"-2.jpg";
+				avatar.alt = users[interested];
+				avatar.className = "mr-1 mb-1";
+			a.appendChild(avatar);
+			$event_interested.append(a);
+		}
+		else
+		{
+			console.warn("Missing interested user "+interested);
+		}
+	}
 }
