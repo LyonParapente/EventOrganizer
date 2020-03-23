LyonParapente/EventOrganizer

commit da399104efc3e10762ca50578ef93024ac047f47

event_show types, fix some bugs! (.extendedProps)

src/event_show.ts
@@ -4,8 +4,9 @@ import { initMap } from './map';
 import loadComments from './event_comments';
 import settings from './settings';
 import { router } from './routing';
+import { EventApi } from '@fullcalendar/core';
 
-export default function showEvent(calEvent)
+export default function showEvent (calEvent: EventApi): void
 {
 	var start = calEvent.start,
 		end = calEvent.end;
@@ -41,11 +42,12 @@ export default function showEvent(calEvent)
 	// Fill event infos
 	$("#event_title").text(calEvent.title);
 	$("#event_description").html(calEvent.extendedProps.desc || i18n('No description'));
-	if (calEvent.category)
+	var category = calEvent.extendedProps.category;
+	if (category)
 	{
-		$("#event_category").text(calEvent.category).css(
+		$("#event_category").text(category).css(
 		{
-			'backgroundColor': getColor(calEvent.category),
+			'backgroundColor': getColor(category),
 			'color': 'white'
 		});
 	}
@@ -85,10 +87,11 @@ export default function showEvent(calEvent)
 	// ----------------------
 	// Activity location
 
-	var event_location_text = calEvent.extendedProps.location || "";
+	var location = calEvent.extendedProps.location;
+	var event_location_text = location || "";
 	var $el = $("#event_location").text(event_location_text);
 	var $el2 = $("#event_location2").text(event_location_text);
-	if (calEvent.location)
+	if (location)
 	{
 		$("#event_location_box").show();
 	}
@@ -100,8 +103,9 @@ export default function showEvent(calEvent)
 	// ----------------------
 	// Rendez-vous location
 
-	$("#event_rdv_time").text(calEvent.extendedProps.time || "");
-	if (calEvent.time)
+	var time = calEvent.extendedProps.time;
+	$("#event_rdv_time").text(time || "");
+	if (time)
 	{
 		$("#event_rdv_time_box").show();
 	}
@@ -111,21 +115,15 @@ export default function showEvent(calEvent)
 	}
 
 	var rdv_location_text = '';
-	if (calEvent.gps || calEvent.gps_location)
+	var gps = calEvent.extendedProps.gps;
+	if (gps)
 	{
-		if (calEvent.gps_location)
-		{
-			rdv_location_text = calEvent.gps_location;
-		}
-		else
-		{
-			rdv_location_text = calEvent.gps.join(', ');
-		}
+		rdv_location_text = gps.join(', ');
 	}
 	else
 	{
 		// Retro-compatibility with old events
-		rdv_location_text = calEvent.location || "";
+		rdv_location_text = location || "";
 	}
 	$("#event_rdv_location").val(rdv_location_text).attr("placeholder", settings.default_location);
 	$("#event_rdv_location_box").show();
@@ -138,7 +136,7 @@ export default function showEvent(calEvent)
 
 	router.navigate("event:"+calEvent.id, i18n("EventTitle", calEvent.id));
 	$eventProperties
-		.one('shown.bs.modal', function()
+		.one('shown.bs.modal', function ()
 		{
 			// Step 1 - Compute textarea height according to width
 			var w = $el.width();
@@ -152,12 +150,12 @@ export default function showEvent(calEvent)
 
 			// -----
 
-			initMap('event_map', false, calEvent.gps, calEvent.location);
+			initMap('event_map', false, gps, location);
 
 			// Avoid keyboard popping on mobile
 			// $("#event_comment").focus();
 		})
-		.one('hide.bs.modal', function()
+		.one('hide.bs.modal', function ()
 		{
 			router.navigate("planning", i18n("Planning"));
 		})

src/main.ts
@@ -13,7 +13,7 @@ import swipedetector from './swipe';
 import { getColor } from './event_plan_categories';
 import { router } from './routing';
 
-export var calendar;
+export var calendar: Calendar;
 
 var id = document.getElementById.bind(document);
 document.addEventListener('DOMContentLoaded', function ()
@@ -216,7 +216,7 @@ function init_routing ()
 			var now = new Date();
 			(function findAndShowEvent()
 			{
-				var event = calendar.getEventById(parseInt(num, 10));
+				var event = calendar.getEventById(num);
 				if (event)
 				{
 					showEvent(event);

commit 9022cdae68cc89164084faa1a71c7b8594926160

Restore something broken by da399104efc3e10762ca50578ef93024ac047f47

src/event_show.ts
@@ -115,14 +115,21 @@ export default function showEvent (calEvent: EventApi): void
 	}
 
 	var rdv_location_text = '';
-	var gps = calEvent.extendedProps.gps;
-	if (gps)
+	var eP = calEvent.extendedProps;
+	if (eP.gps || eP.gps_location)
 	{
-		rdv_location_text = gps.join(', ');
+		if (eP.gps_location)
+		{
+			rdv_location_text = eP.gps_location;
+		}
+		else
+		{
+			rdv_location_text = eP.gps.join(', ');
+		}
 	}
 	else
 	{
-		// Retro-compatibility with old events
+		// Retro-compatibility with old events that don't have .gps or .gps_location
 		rdv_location_text = location || "";
 	}
 	$("#event_rdv_location").val(rdv_location_text).attr("placeholder", settings.default_location);
@@ -151,7 +158,7 @@ export default function showEvent (calEvent: EventApi): void
 
 			// -----
 
-			initMap('event_map', false, gps, location);
+			initMap('event_map', false, eP.gps, location);
 
 			// Avoid keyboard popping on mobile
 			// $("#event_comment").focus();