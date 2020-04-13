4b9f0724c141c0f7383ec22398e10c9979ea01ec
TiTi <anthibug@gmail.com>
2020-04-13 23:35:58 +0200

    Fix carriage return in comments

--- front/src/event_comments.ts
+++ front/src/event_comments.ts
@@ -113,7 +113,7 @@ function createCommentEntry (comment: Comment, userid: number, user: User): HTML
 			var p = document.createElement("p");
 			p.className = 'blockquote my-1 text-dark';
 			p.appendChild(document.createTextNode(comment.comment));
-			p.innerHTML = p.innerHTML.replace(/\n/g, '<br/>');
+			p.innerHTML = p.innerHTML.replace(/\\n/g, '<br/>');
 		col.appendChild(p);
 	groupitem.appendChild(col);
 	return groupitem;
