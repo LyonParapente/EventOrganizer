92dd9e7d6876dcb7e854d4e17afb01c6bf854d44
TiTi <anthibug@gmail.com>
2020-04-13 17:57:00 +0200

    Catch exception when UNIQUE email constraint is broken at sql level

--- back/api/user/create.py
+++ back/api/user/create.py
@@ -1,7 +1,8 @@
-from flask import request
+from flask import request, abort
 from flask_restful_swagger_3 import Resource, swagger
 from models.user import User, get_user_parser
 from database.manager import db
+import sqlite3
 
 class UserAPICreate(Resource):
   post_parser = get_user_parser()
@@ -25,13 +26,23 @@ class UserAPICreate(Resource):
             'schema': User
           }
         }
+      },
+      '409': {
+        'description': 'Email already registered'
       }
     }
   })
   def post(self):
     """Create a user"""
     args = self.post_parser.parse_args(strict=True)
-    user = db.insert_user(**args)
+    try:
+      user = db.insert_user(**args)
+    except sqlite3.IntegrityError as err:
+      if str(err) == "UNIQUE constraint failed: users.email":
+        abort(409, 'Email already registered')
+      else:
+        raise
+
     for field in User.always_filtered:
       user[field] = None
     streamlined_user = {k: v for k, v in user.items() if v is not None}
