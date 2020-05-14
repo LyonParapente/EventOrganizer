ed091002c00c4c6a23ba708d4a6ba38f86e86fd5
TiTi <anthibug@gmail.com>
2020-05-14 18:37:43 +0200

    Wait that window is fully loaded (especially styles) to have the correct height, especially in /settings

--- front/src/unsplash.ts
+++ front/src/unsplash.ts
@@ -1,11 +1,16 @@
-export default function unsplash (tags)
+export default function unsplash (tags?)
 {
-  var tags = tags.join(',');
   const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
   const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
   var resolution = vw + "x" + vh;
-  // var url = `https://source.unsplash.com/featured/${resolution}?${tags}`;
+  // var url = `https://source.unsplash.com/featured/${resolution}?${tags.join(',')}`;
   var url = `/background/${resolution}`;
   document.body.style.backgroundImage = "url('"+url+"')"
 }
-(<any>window).unsplash = unsplash;
+
+function unsplashL ()
+{
+  window.addEventListener('load', unsplash);
+}
+
+(<any>window).unsplashL = unsplashL;
