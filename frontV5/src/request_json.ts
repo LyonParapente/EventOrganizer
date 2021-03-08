9d83e68974c353c3d14a8a1e76988daa58d6537e
TiTi <anthibug@gmail.com>
2021-03-09 00:26:46 +0100

    Add all the code

---
+++ frontV5/src/request_json.ts
@@ -0,0 +1,69 @@
+var csrf;
+var csrf_cookies = document.cookie.split('; ')
+  .map(x => x.split('='))
+  .filter(x => x[0] === 'csrf_access_token');
+if (csrf_cookies.length)
+{
+  csrf = csrf_cookies[0][1];
+}
+
+export default function requestJson (method: string, url: string, params: object, successCallback, failureCallback) {
+  method = method.toUpperCase();
+  var body = null;
+  if (method === 'GET') {
+    url = injectQueryStringParams(url, params);
+  }
+  else {
+    body = JSON.stringify(params);
+  }
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url, true);
+  if (method !== 'GET') {
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    if (csrf)
+    {
+      xhr.setRequestHeader('X-CSRF-TOKEN', csrf);
+    }
+  }
+  xhr.onload = function () {
+    if (xhr.status >= 200 && xhr.status < 400) {
+      var res;
+      try {
+        res = JSON.parse(xhr.responseText);
+      }
+      catch (err) {
+        failureCallback('Failure parsing JSON: '+err.message, xhr);
+      }
+      if (res)
+      {
+        successCallback(res, xhr);
+      }
+    }
+    else {
+      failureCallback('Request failed', xhr);
+    }
+  };
+  xhr.onerror = function () {
+    failureCallback('Request failed', xhr);
+  };
+  xhr.send(body);
+}
+
+function injectQueryStringParams (url: string, params: object) {
+  var parameters = encodeParams(params);
+  if (parameters)
+  {
+    return url + (url.indexOf('?') === -1 ? '?' : '&') + parameters;
+  }
+  return url;
+}
+
+function encodeParams (params: object) {
+  var parts = [];
+  for (var key in params) {
+    if (params.hasOwnProperty(key)) {
+      parts.push(encodeURIComponent(key) + '=' + encodeURIComponent(params[key]));
+    }
+  }
+  return parts.join('&');
+}
---
+++ frontV5/src/unsplash.ts
@@ -0,0 +1,16 @@
+export default function unsplash (tags=[], extraPath='')
+{
+  const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  var resolution = vw + "x" + vh;
+  // var url = `https://source.unsplash.com/featured/${resolution}?${tags.join(',')}`;
+  var url = `/background${extraPath}/${resolution}?${tags.join(',')}`;
+  document.body.style.backgroundImage = "url('"+url+"')"
+}
+
+function unsplashL ()
+{
+  window.addEventListener('load', x => unsplash.apply(this, arguments));
+}
+
+(<any>window).unsplashL = unsplashL;
