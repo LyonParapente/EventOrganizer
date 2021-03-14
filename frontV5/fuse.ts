72d736f4cb536eaf760ef2ed70cdcc8b48f16637
TiTi <anthibug@gmail.com>
2021-03-14 17:01:02 +0100

    Make /api/events and /api/messages work with some fake data

--- frontV5/fuse.ts
+++ frontV5/fuse.ts
@@ -1,9 +1,28 @@
 import { fusebox } from 'fuse-box';
+import * as path from 'path';
 
 const fuse = fusebox({
   entry: 'src/main.ts',
   target: 'browser',
-  devServer: true,
+  devServer: {
+    // open: true,
+    httpServer: {
+      express: (app, express) => {        
+        app.get('/api/events', function (req, res)
+        {
+          var eventsJson = path.join(__dirname, 'data/events/2021.json');
+          var readable = require('fs').createReadStream(eventsJson);
+          readable.pipe(res);
+        });
+        app.get('/api/messages', function (req, res)
+        {
+          var eventsJson = path.join(__dirname, 'data/events/Event_'+req.query.event_id+'.json');
+          var readable = require('fs').createReadStream(eventsJson);
+          readable.pipe(res);
+        });
+      }
+    }
+  },
   webIndex: {template: 'src/calendar.html'}
 });
 
