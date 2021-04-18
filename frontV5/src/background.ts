fcfcf4bc77ad0e10447816cc06ede39eedbfc4f0
TiTi <anthibug@gmail.com>
2021-04-19 00:10:44 +0200

    Indent like other files

--- frontV5/src/background.ts
+++ frontV5/src/background.ts
@@ -1,22 +1,22 @@
 export function background (use_unsplash: boolean, tags: string[]=[], force: boolean=false): void
 {
-  const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-  const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-  var resolution = vw + "x" + vh;
-  var url;
-  if (use_unsplash)
-  {
-    url = `https://source.unsplash.com/featured/${resolution}?${tags.join(',')}`;
-  }
-  else
-  {
-    url = `/background/${resolution}`;
-  }
-  if (force)
-  {
-    url += (url.includes("?") ? "&" : "?") + "rand=" + new Date().getTime().toString();
-  }
-  document.body.style.backgroundImage = "url('"+url+"')"
+	const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+	const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+	var resolution = vw + "x" + vh;
+	var url;
+	if (use_unsplash)
+	{
+		url = `https://source.unsplash.com/featured/${resolution}?${tags.join(',')}`;
+	}
+	else
+	{
+		url = `/background/${resolution}`;
+	}
+	if (force)
+	{
+		url += (url.includes("?") ? "&" : "?") + "rand=" + new Date().getTime().toString();
+	}
+	document.body.style.backgroundImage = "url('"+url+"')"
 }
 
 export function setBackgroundColor (calendarEl: HTMLElement)
